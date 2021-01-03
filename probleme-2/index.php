0a1c34afc908477d2ce564e363596728d602160d
Zawatsuka <thomaslucas2904@gmail.com>
2021-01-03 18:00:12 +0100

    exercice 2 fait

--- probleme-2/index.php
+++ probleme-2/index.php
@@ -2,3 +2,12 @@
 ==========
 Je dois carreler mon séjour qui mesure 6m*3m. Les dalles font 33cm*33cm. Les carrelages à acheter se vendent par 10.
 Développer un outil permettant de me donner le nombre de paquets à acheter. -->
+<?php 
+    function slabCount($longSide , $smallOdds , $nbreOfSlab , $longSideSlabInMeter , $smallOddsSlabInMeter){
+        $totalRoom = $longSide * $smallOdds;
+        $totalPackInSlab = ($longSideSlabInMeter * $smallOddsSlabInMeter)*$nbreOfSlab;
+        $nbreTotalPack = floor($totalRoom * $totalPackInSlab);
+        return($nbreTotalPack);
+    }
+    echo slabCount(6 , 3 , 10 , 0.33 , 0.33);
+?>
